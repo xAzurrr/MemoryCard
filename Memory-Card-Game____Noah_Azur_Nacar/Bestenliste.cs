8ca9a0030c93af7c29cb7c25f0f35bd973c98deb
xAzurrr <noah_is_noah@web.de>
2025-07-24 20:05:19 +0200

    Initial commit

---
+++ Memory-Card-Game____Noah_Azur_Nacar/Bestenliste.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Card_Game____Noah_Azur_Nacar
+{
+    internal class Bestenliste
+    {
+        private string  spielername;
+        private int     verbleibendeLeben;
+        private string  noetigeZeit;
+
+        private List<Bestenliste> ergebnisse = new List<Bestenliste>();
+
+        public string get_spielername()
+        {
+            return spielername;
+        }
+
+        public void set_spielername(string _spielername)
+        {
+            spielername = _spielername;
+        }
+
+        public int get_verbleibendeLeben()
+        {
+            return verbleibendeLeben;
+        }
+
+        public void set_verbleibendeLeben(int _verbleibendeLeben)
+        {
+            verbleibendeLeben = _verbleibendeLeben;
+        }
+
+        public string get_noetigeZeit()
+        {
+            return noetigeZeit;
+        }
+
+        public void set_noetigeZeit(string _noetigeZeit)
+        {
+            noetigeZeit = _noetigeZeit;
+        }
+
+        public void ergebnisHinzufuegen(Bestenliste ergebnis)
+        {
+            ergebnisse.Add(ergebnis);
+        }
+
+        public List<Bestenliste> get_ganzeErgebnisse()
+        {
+            return ergebnisse;
+        }
+
+        public void speichern(string dateipfad)
+        {
+            using (StreamWriter writer = new StreamWriter(dateipfad))
+            {
+                foreach (var erg in ergebnisse)
+                {
+                    writer.WriteLine(erg.get_spielername() + ";" + erg.get_verbleibendeLeben() + ";" + erg.get_noetigeZeit());
+                }
+            }
+        }
+
+        public void laden(string dateipfad)
+        {
+            ergebnisse.Clear();
+            if (!File.Exists(dateipfad)) return;
+
+            var zeilen = File.ReadAllLines(dateipfad);
+            foreach (var zeile in zeilen)
+            {
+                var teile = zeile.Split(";");
+                if (teile.Length == 3)
+                {
+                    var ergebnis = new Bestenliste();
+                    ergebnis.set_spielername(teile[0]);
+                    ergebnis.set_verbleibendeLeben(int.Parse(teile[1]));
+                    ergebnis.set_noetigeZeit(teile[2]);
+                    ergebnisse.Add(ergebnis);
+                }
+            }
+        }
+    }
+}
---
+++ Memory-Card-Game____Noah_Azur_Nacar/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory_Card_Game____Noah_Azur_Nacar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Einstellungen spielEinstellungen;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            spielEinstellungen = new Einstellungen(10,10,0,0);
+            
+
+        }
+        public MainWindow(Einstellungen _spielEinstellungen)
+        {
+            InitializeComponent();
+            spielEinstellungen = _spielEinstellungen;
+
+        }
+
+        private void btnSpielBeenden_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSpielBestenListe_Click(object sender, RoutedEventArgs e)
+        {
+            BestenlisteWindow bestenliste = new BestenlisteWindow(spielEinstellungen);
+            bestenliste.Show();
+            this.Close();
+        }
+
+        private void btnSpielOptionen_Click(object sender, RoutedEventArgs e)
+        {
+            OptionenWindow optionenWindow = new OptionenWindow(spielEinstellungen);
+            optionenWindow.Show();
+            this.Close();
+
+        }
+
+        private void btnSpielStarten_Click(object sender, RoutedEventArgs e)
+        {
+
+            if(spielEinstellungen.spielKartenAnzahl == 20)
+            {
+                SpielZwanzigKartenWindow spielzwanzigkartenWindow = new SpielZwanzigKartenWindow(spielEinstellungen);
+                spielzwanzigkartenWindow.Show();
+                this.Close();
+            }
+            else if(spielEinstellungen.spielKartenAnzahl==10)
+            {
+                SpielZehnKartenWindow spielZehnKartenWindow = new SpielZehnKartenWindow(spielEinstellungen);
+                spielZehnKartenWindow.Show();
+                this.Close();
+            }
+
+
+        }
+    }
+}
